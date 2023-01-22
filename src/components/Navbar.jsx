1eeb68ffdd743f72b0efd279155c1b85d706cba7
Aarush_009 <aarushkumarchoubey@gmail.com>
2023-01-22 22:57:47 +0530

    2nd commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
+import { Search } from "@mui/icons-material";
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import { logo } from "../utils/constants";
+
+import SearchBar from "./SearchBar";
 
 const Navbar = () => (
   <Stack
@@ -18,6 +21,7 @@ const Navbar = () => (
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={40} />
     </Link>
+    <SearchBar />
   </Stack>
 );
 
